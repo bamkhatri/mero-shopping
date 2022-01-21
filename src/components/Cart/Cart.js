f9273859896ca60ae6e55682d828e471459cedb4
bamkhatri <bamkhatri24@gmail.com>
2022-01-21 21:01:19 +0545

    link is added to checkout button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import cartSlice from '../../features/slices/cartSlice'
+
 import {
   removeFromCart,
   decreaseCartQuantity,
