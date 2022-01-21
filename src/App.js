586deaecf40c7899dca3beb2a7af48331874e8bc
bamkhatri <bamkhatri24@gmail.com>
2022-01-21 09:55:16 +0545

    minor change in header and footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 
@@ -7,7 +8,9 @@ function App() {
     <>
       <Header />
       <main>
-        <h1>Welcome to Mero Shopping</h1>
+        <Container>
+          <h1>Welcome to Mero Shopping</h1>
+        </Container>
       </main>
       <Footer />
     </>
