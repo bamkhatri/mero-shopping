9ce80375229103ff8ec03ed9c79aef3e84f01fad
bamkhatri <bamkhatri24@gmail.com>
2022-01-21 20:13:13 +0545

    minor change in header to show product quantity

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  const { totalProduct } = useSelector((state) => state.cartSlice)
+
   return (
     <header>
       <div className='header'>
@@ -14,7 +17,7 @@ const Header = () => {
           <div className='nav-bag'>
             <i className='fas fa-shopping-cart'></i> Cart
             <span className='cart-quantity'>
-              <span>3</span>
+              <span>{totalProduct}</span>
             </span>
           </div>
         </Link>
