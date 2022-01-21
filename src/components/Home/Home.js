5a0b1522ad7a1d4566dcf34636bfecf420117255
bamkhatri <bamkhatri24@gmail.com>
2022-01-21 14:07:02 +0545

    render product to the home page is complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,27 +2,39 @@ import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProduct } from '../../features/slices/productSlice'
-
+import './Home.css'
 const Home = () => {
+  const image = 'https://electronic-ecommerce.herokuapp.com/fantechHeadset.jpg'
   const [datas, setDatas] = useState([])
   const dispatch = useDispatch()
 
-  const data = useSelector((state) => state.product.data)
-  console.log(data)
+  const productStore = useSelector((state) => state.product.data)
+
   useEffect(() => {
     if (datas.length === 0) {
       dispatch(fetchProduct())
     }
-    setDatas(data)
-  }, [data])
+    setDatas(productStore)
+  }, [productStore])
   return (
     <main>
-      <Container>
-        <h1>Welcome to Mero Shopping</h1>
-        {datas.map((data) => {
-          const { id, name } = data
-          return <div key={id}>{name}</div>
-        })}
+      <Container className='home-container'>
+        <h2>Welcome to Mero Shopping</h2>
+        <div className='render-products'>
+          {datas?.map((product) => {
+            return (
+              <div key={product.id} className='render-product'>
+                <img src={image} alt='img' />
+                <h3>{product.name}</h3>
+                <div className='product-details'>
+                  <span className='product-price'>Price: {product.price}</span>
+                  <span>Stock: {product.stock}</span>
+                </div>
+                <button>Add to Cart</button>
+              </div>
+            )
+          })}
+        </div>
       </Container>
     </main>
   )
